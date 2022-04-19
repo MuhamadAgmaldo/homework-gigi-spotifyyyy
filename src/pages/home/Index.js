489266f3c3440986a075d2b69f3f52a2b2e4e25b
MuhamadAgmaldo <muhammadagmaldo05@gmail.com>
2022-04-19 22:41:34 +0700

    move to typescript

---
+++ src/pages/home/Index.js
@@ -0,0 +1,38 @@
+import "./Home.css";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addToken } from "../../redux/auth-slice";
+
+function Home() {
+  const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+  const REDIRECT_URI = "http://localhost:3000/create-playlist";
+  const SCOPE = "playlist-modify-private";
+  const AUTH_URL = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&response_type=token&show_dialog=true`;
+  const dispatch = useDispatch();
+
+  const isAuth = () => {
+    window.location = AUTH_URL;
+    localStorage.setItem("isLoggedIn", true);
+  };
+
+  
+  return (
+    <div className="Home">
+      <div className="header"></div>
+      <div className="content">
+        <p className="title">
+          Klik button untuk login 
+        </p>
+        <button type="submit" onClick={isAuth}>
+          Click here to login
+        </button>
+      </div>
+      <div className="footer">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Home;
