58ec47369f041c2099ceb871ff3b2a7a14317c74
MuhamadAgmaldo <muhammadagmaldo05@gmail.com>
2022-04-22 11:11:29 +0700

    testing deployyyy

--- src/pages/home/Index.js
+++ src/pages/home/Index.js
@@ -1,22 +1,32 @@
 import "./Home.css";
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { addToken } from "../../redux/auth-slice";
+import { addToken, isLogin } from "../../redux/auth-slice";
+import { selectToken } from "../../redux/auth-slice";
+import { Route, Redirect, Link } from "react-router-dom";
+import Spotify from "../spotify/Spotify";
 
 function Home() {
-  const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const SPOTIFY_AUTHORIZE_ENDPOINT = process.env.REACT_APP_ENDPOINT;
   const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-  const REDIRECT_URI = "https://homework-gigi-spotifyyyy.vercel.app/";
+  const REDIRECT_URI = process.env.REACT_APP_REDIRECT;
   const SCOPE = "playlist-modify-private";
   const AUTH_URL = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&response_type=token&show_dialog=true`;
   const dispatch = useDispatch();
-
+  const getToken = new URLSearchParams(window.location.hash).get(
+    "#access_token"
+  );
+  const accessToken = useSelector(selectToken);
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
   const isAuth = () => {
     window.location = AUTH_URL;
     localStorage.setItem("isLoggedIn", true);
   };
 
-  
+  useEffect(() => {
+    dispatch(addToken(getToken));
+  },[]);
+
   return (
     <div className="Home">
       <div className="header"></div>
